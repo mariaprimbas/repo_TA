d01674f67a558839dc22c4e8dbbd2a851df3eefe
mariaprimbas <primbasmaria@gmail.com>
2026-06-18 16:08:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ while True:
 
     elif opcion == 3:
 
-        print("Comparando segmentos...")
+        print("\nComparando segmentos...\n")
         analisis = segmentos(df)
             
         mostrar_dataframe(analisis.reset_index(),"\nComparación de segmentos")
@@ -70,7 +70,7 @@ while True:
 
         cliente= crear_cliente_id(df_valido, id_cliente)
 
-        print("Generando recomendaciones...")
+        print("\n\nGenerando recomendaciones...\n")
         reco = generar_recomendacion_api(cliente)
         print(reco)
         
